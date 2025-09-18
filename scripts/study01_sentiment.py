98e1414ba7c0de83bbad5e28fd0df443b61ed929
Lukas Gardeweg <lu.garde54@gmail.com>
2025-09-18 13:51:03 +0200

    Changes

--- scripts/study01_sentiment.py
+++ scripts/study01_sentiment.py
@@ -1,6 +1,11 @@
 import pandas as pd
 
-# Beispiel: 4 CSV-Dateien
-datasets = {}
-for name in ["01_across_datasource", "02_across_samples", "03_across_time", "04_reproduction"]:
-    datasets[name] = pd.read_csv(f"{name}.xls")
+base_url = "https://github.com/LukasGardeweg/semi-strong-market-efficiency-replication.git"
+files = [
+    "01_across_datasource.csv",
+    "02_across_samples.csv",
+    "03_across_time.csv",
+    "04_reproduction.csv"
+]
+
+datasets = {file.split(".")[0]: pd.read_csv(base_url + file) for file in files}
